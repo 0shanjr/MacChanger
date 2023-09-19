86751f9a07384abece579939d3ac29c5419d8be0
OSHAN JR <79368912+0shanjr@users.noreply.github.com>
2023-09-20 05:20:12 +0530

    Updated mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,17 +1,13 @@
 #/user/bin/env/python
+#by oshanjr
 
 import  subprocess
 
 new_mac = input("Enter your New Mac Address =")
 
-# subprocess.call("ifconfig",shell=True)
 print("*****************************")
 print("[+]changing your Eth0 mac address to = " + new_mac)
 print("*****************************")
-
-# subprocess.call("ifconfig eth0 down",shell=True)
-# subprocess.call("ifconfig eth0 hw ether " + new_mac ,shell=True)
-# subprocess.call("ifconfig eth0 up",shell=True)
 
 subprocess.run(["ifconfig", "eth0", "down"])
 subprocess.run(["ifconfig", "eth0", "hw","ether", new_mac])
